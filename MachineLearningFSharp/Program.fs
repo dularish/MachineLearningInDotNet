5bf29bb44fe4d000f70a6400d74019bf1c0a770c
Dularish Kuttuwa <dularish1993@gmail.com>
2019-08-06 19:47:53 +0200

    Added Intel MKL nuget package

--- MachineLearningFSharp/Program.fs
+++ MachineLearningFSharp/Program.fs
@@ -1,12 +1,11 @@
 ﻿open MathNet.Numerics.LinearAlgebra
 open LogisticRegression
 open OpenCvSharp
-open OpenCvSharp
-open OpenCvSharp
-open OpenCvSharp
-open OpenCvSharp
-open OpenCvSharp
 open OpenCVUtils
+open MathNet.Numerics
+open MathNet.Numerics.LinearAlgebra
+open System.Timers
+open System.Diagnostics
 
 // Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
@@ -16,19 +15,37 @@ let showEachImageFromDB = fun(X:Matrix<double>) ->
         let imageData = X.Column(i).ToArray() |> Array.map (fun(x) -> (byte)x)
         OpenCVUtils.showImageUntilKeyEntered (imageData) ("Image : " + i.ToString()) 3
 
+let compareProviderPerformance = fun() ->
+    let train_x,test_x,train_y, test_y = MLDatasets.load_catsDatasets()
+
+    
+    Control.UseNativeMKL()
+    let timer = Stopwatch()
+    timer.Start()
+    let model = LogisticRegression.createModel train_x train_y test_x test_y 2000 0.0075
+    timer.Stop()
+    printfn "NativeMKL timer clock : %A" timer.Elapsed.TotalSeconds
+    Control.UseManaged()
+    
+    timer.Restart()
+    let model2 = LogisticRegression.createModel train_x train_y test_x test_y 2000 0.0075
+    timer.Stop()
+    printfn "ManagedProvider timer clock : %A" timer.Elapsed.TotalSeconds
+
+
+
+    
+
 [<EntryPoint>]
 let main argv = 
-    
-    let train_x,test_x,train_y, test_y = MLDatasets.load_catsDatasets()
+    Control.UseNativeMKL()
+    //let train_x,test_x,train_y, test_y = MLDatasets.load_catsDatasets()
     //showEachImageFromDB(train_x)
-    //printfn "%A" train_x
-    //printfn "%A" train_y
-    //printfn "%A" test_x
-    //printfn "%A" test_y
-    //printfn "%A" initialize_dataset
-
+    
     //let model = LogisticRegression.createModel train_x train_y test_x test_y 2000 0.005
-    let model = NeuralNetwork.createModel train_x train_y test_x test_y 2000 0.0075
+    //let model = NeuralNetwork.createModel train_x train_y test_x test_y 2000 0.0075
+
+    compareProviderPerformance()
 
     System.Console.ReadKey() |> ignore
     printfn "%A" argv
