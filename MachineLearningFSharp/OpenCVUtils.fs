fdac3472869488c0527f9fdfc4549ef990dfa29c
Dularish Kuttuwa <dularish1993@gmail.com>
2020-03-18 21:30:55 +0530

    Modified logic for opencv image display

--- MachineLearningFSharp/OpenCVUtils.fs
+++ MachineLearningFSharp/OpenCVUtils.fs
@@ -5,7 +5,8 @@ open OpenCvSharp
 //The input is a three channel data in which data is provided channel wise that is first red channel data for all the pixel followed by other pixels
 let showImageUntilKeyEntered (imageData:byte[]) (windowName:string) (number_of_color_channels:int) =
     let pixels_number = System.Math.Sqrt((double)imageData.Length/(double)number_of_color_channels) |> int
-    let someImage = new Mat(pixels_number,pixels_number, MatType.CV_8UC3, imageData)
+    let mutable someImage = new Mat(pixels_number,pixels_number, MatType.CV_8UC3, imageData)
+    someImage <- someImage.CvtColor(ColorConversionCodes.RGB2BGR)//OpenCV needs BGR for display
     Cv2.NamedWindow(windowName, WindowMode.Normal)
     Cv2.ResizeWindow(windowName,600,600)
     Cv2.ImShow(windowName, someImage)
